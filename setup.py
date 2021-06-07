c9442e67d7c4e413aebf2a4cc7ce0ccf799b5c7f
Susanna Kiwala <susanna.kiwala@wustl.edu>
2021-06-07 09:36:34 -0500

    Install MHCflurry 2.0.2 for compatibility with later tensorflow versions

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
         'vaxrank>=1.1.0',
         'tensorflow==2.2.2',
         'mhcnuggets==2.3.3',
-        'mhcflurry==2.0.1',
+        'mhcflurry==2.0.2',
     ],
     package_data={
         'tools.pvacseq': pvacseq_data_files,
